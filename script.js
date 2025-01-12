ae1735d5e7d8e0914d4d05bf27ce0bc1f7690fbe
Alex <alex.real1850@gmail.com>
2025-01-12 22:58:14 +0200

    lesson_18 final commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //* 1
-const numberOfFilms = prompt('How many movies have you watched?', '');
+const numberOfFilms = +prompt('How many movies have you watched?', '');
 
 //* 2
 const personalMovieDB = {
@@ -12,10 +12,10 @@ const personalMovieDB = {
 
 //* 3
 const firstWatchedMovie = prompt('One of the last watched movie?', '');
-const firstMovieRate = prompt('How would you rate it?', '');
+const firstMovieRate = +prompt('How would you rate it?', '');
 const secondWatchedMovie = prompt('One of the last watched movie?', '');
-const secondMovieRate = prompt('How would you rate it?', '');
+const secondMovieRate = +prompt('How would you rate it?', '');
 
-personalMovieDB.movies[`${firstWatchedMovie}`] = firstMovieRate;
-personalMovieDB.movies[`${secondWatchedMovie}`] = secondMovieRate;
+personalMovieDB.movies[firstWatchedMovie] = firstMovieRate;
+personalMovieDB.movies[secondWatchedMovie] = secondMovieRate;
 console.log(personalMovieDB);
