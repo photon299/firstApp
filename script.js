ff6641f858cb4879bf8f5f4db6ccd0c669f55210
Alex <alex.real1850@gmail.com>
2025-01-12 22:51:19 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//* 1
+const numberOfFilms = prompt('How many movies have you watched?', '');
+
+//* 2
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+//* 3
+const firstWatchedMovie = prompt('One of the last watched movie?', '');
+const firstMovieRate = prompt('How would you rate it?', '');
+const secondWatchedMovie = prompt('One of the last watched movie?', '');
+const secondMovieRate = prompt('How would you rate it?', '');
+
+personalMovieDB.movies[`${firstWatchedMovie}`] = firstMovieRate;
+personalMovieDB.movies[`${secondWatchedMovie}`] = secondMovieRate;
+console.log(personalMovieDB);
