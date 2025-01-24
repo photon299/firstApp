488d78fde990015949f31b15fd38dd3d5410aef6
Alex <alex.real1850@gmail.com>
2025-01-25 00:05:18 +0200

    lesson 36 small commit: my try

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict"
 
 let numberOfFilms;
-
 function start() {
     numberOfFilms  = +prompt('How many movies have you watched?', '');
     while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
@@ -16,38 +15,38 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false,
-};
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        let watchedMovie = prompt('One of the last watched movie?', '').trim();
-        let rating = prompt('How would you rate it?', '');
-        if (watchedMovie !== "" && rating !== "" && watchedMovie !== null && rating !== null && watchedMovie.length < 50) {
-            personalMovieDB.movies[watchedMovie] = rating;
-        } else {
-            i--;
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            let watchedMovie = prompt('One of the last watched movie?', ''); //.trim()
+            let rating = prompt('How would you rate it?', '');
+            if (watchedMovie !== "" && rating !== "" && watchedMovie !== null && rating !== null && watchedMovie.length < 50) {
+                this.movies[watchedMovie] = rating;
+            } else {
+                i--;
+            }
         }
-    }
-}
-rememberMyFilms();
+    },
+    detectPersonalLevel: function() {
+        alert((this.count < 10) ? "Watched quite a few movies" :
+        (this.count < 30) ? "You're a classic viewer" :
+        (this.count >= 30) ? "You're a movie fan" : "Error");
+    },
+    writeYourGenres: function() {
+        for (let i = 0; i < 3; i++) {
+            this.genres[i] = prompt(`Your favorite genre number ${i + 1}`, ``);
+            if (typeof this.genres[i] !== "string" || this.genres[i].length === 0) i--;
+        }
+    },
+    toggleVisibleMyDB: function() {
+        this.privat = (!this.privat) ? true : false;
+    },
+    showMyBD: function(hidden) {
+        if (hidden) console.log(this);
+    },
+};
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.writeYourGenres();
 
-function detectPersonalLevel (){
-    alert((personalMovieDB.count < 10) ? "Watched quite a few movies" :
-    (personalMovieDB.count < 30) ? "You're a classic viewer" :
-    (personalMovieDB.count >= 30) ? "You're a movie fan" : "Error");
-}
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) console.log(personalMovieDB);
-}
-
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`Your favorite genre number ${i + 1}?`, ``);;
-    }
-}
-writeYourGenres();
-
-showMyDB(personalMovieDB.privat);
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyBD(personalMovieDB.privat);
